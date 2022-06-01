59f62320fe9c0987ed5bc59df6e0965742d69f8c
gustavo alef <gustalef@gmail.com>
2022-06-01 10:07:39 -0300

    capturando evento dos botoes e mostrando-os na tela

--- calculadora/assets/scripts/calculadora/CalculadoraControle.js
+++ calculadora/assets/scripts/calculadora/CalculadoraControle.js
@@ -5,7 +5,46 @@ export default class calculadoraControle {
     constructor(tela = new Tela()) {
         this.tela = tela;
         new DataHora();
-        this.tela.conteudo = "1.5";
+        this.eventosBotoes();
+    }
+    eventosBotoes() {
+        document.querySelectorAll("#teclado button").forEach((elemento) => {
+            elemento.addEventListener("click", (evento) => {
+                const target = evento.target;
+                switch (target.id) {
+                    case "zero":
+                    case "um":
+                    case "dois":
+                    case "tres":
+                    case "quatro":
+                    case "cinco":
+                    case "seis":
+                    case "sete":
+                    case "oito":
+                    case "nove":
+                        this.adicionarNumero(Number(target.dataset.valor));
+                        break;
+                    case "adicao":
+                    case "subtracao":
+                    case "multiplicacao":
+                    case "divisao":
+                        break;
+                    case "ponto":
+                        break;
+                    case "limpar":
+                        break;
+                    case "desfazer":
+                        break;
+                    case "porcentagem":
+                        break;
+                    case "igual":
+                        break;
+                }
+            });
+        });
+    }
+    adicionarNumero(numero) {
+        this.tela.conteudo = numero.toString();
     }
 }
 //# sourceMappingURL=CalculadoraControle.js.map
--- calculadora/assets/typescript/calculadora/CalculadoraControle.ts
+++ calculadora/assets/typescript/calculadora/CalculadoraControle.ts
@@ -5,6 +5,55 @@ export default class calculadoraControle {
   constructor(private tela = new Tela()) {
     new DataHora();
 
-    this.tela.conteudo = "1.5";
+    this.eventosBotoes();
+  }
+
+  eventosBotoes(): void {
+    document.querySelectorAll("#teclado button").forEach((elemento) => {
+      elemento.addEventListener("click", (evento: Event) => {
+        //distinguindo os botoes apertados
+        const target = evento.target as HTMLButtonElement;
+
+        switch (target.id) {
+          case "zero":
+          case "um":
+          case "dois":
+          case "tres":
+          case "quatro":
+          case "cinco":
+          case "seis":
+          case "sete":
+          case "oito":
+          case "nove":
+            this.adicionarNumero(Number(target.dataset.valor))
+            break;
+
+          case "adicao":
+          case "subtracao":
+          case "multiplicacao":
+          case "divisao":
+            break;
+
+          case "ponto":
+            break;
+
+          case "limpar":
+            break;
+
+          case "desfazer":
+            break;
+
+          case "porcentagem":
+            break;
+
+          case "igual":
+            break;
+        }
+      });
+    });
+  }
+
+  adicionarNumero(numero: number): void {
+    this.tela.conteudo = numero.toString();
   }
 }
